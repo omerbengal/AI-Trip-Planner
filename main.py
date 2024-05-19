765145703c8a776411bee4903970ccd8df58ae67
Omer Bengal <omer.bengal01@post.runi.ac.il>
2024-05-19 23:04:15 +0300

    chore: add dalle image generation functionality

--- main.py
+++ main.py
@@ -4,45 +4,27 @@ import json
 from serpapi import GoogleSearch
 import requests
 from openai import OpenAI
+import unicodedata
 
-SERPAPI_KEY = '66051b9cf17d136b33dd0cdc73eddaab28fe9b7d5967bf6cc3c85d4d6e9d416d'
+SERPAPI_KEY = '3430d3c96668875ecbffc4dfa2c46f373322f1b6825540394a11355701c75160'
+OPENAI_CLIENT = OpenAI(
+    api_key="sk-proj-5hGNQsFq3ncGG0V3vcOeT3BlbkFJej39qWF8lI2qLnJqTOjt"
+)
 
 
-def send_prompt2(prompt):
-    client = OpenAI(
-        api_key="sk-proj-5hGNQsFq3ncGG0V3vcOeT3BlbkFJej39qWF8lI2qLnJqTOjt"
-    )
-    completion = client.chat.completions.create(
+def send_prompt(prompt) -> str:
+    completion = OPENAI_CLIENT.chat.completions.create(
         model="gpt-4o",
         messages=[
             {"role": "system", "content": "You are an experienced worldwide vacation planner. Additionally you are familiar with airports around the world."},
             {"role": "user", "content": prompt}
         ]
     )
-    return completion.choices[0].message.content
-
-
-def send_prompt(prompt):
-    url = "https://api.openai.com/v1/chat/completions"
-    headers = {
-        "Content-Type": "application/json",
-        # Replace YOUR_API_KEY with your actual OpenAI API key
-        "Authorization": "Bearer sk-proj-5hGNQsFq3ncGG0V3vcOeT3BlbkFJej39qWF8lI2qLnJqTOjt"
-    }
-    data = {
-        "model": "gpt-4",
-        "messages": [{"role": "user", "content": prompt}]
-    }
-
-    try:
-        response = requests.post(url, headers=headers, data=json.dumps(data))
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        response_data = response.json()
-        return response_data['choices'][0]['message']['content']
-    except requests.exceptions.HTTPError as http_err:
-        return f"HTTP error occurred: {http_err}"
-    except Exception as err:
-        return f"An error occurred: {err}"
+    response = completion.choices[0].message.content
+    if response is None:
+        return ''
+    else:
+        return response
 
 
 def get_top_5_destinations(start_month: int, end_month: int, trip_type: str):
@@ -63,7 +45,7 @@ def get_top_5_destinations(start_month: int, end_month: int, trip_type: str):
         12: "December"
     }
 
-    response = send_prompt2(f"""
+    response = send_prompt(f"""
     Given the start month {months[start_month]} and end month {months[end_month]}, generate a list of the top 10 destinations for a {trip_type} vacation in the given time of the year.\n
     Do not include any other information in your response.
     The format should be:
@@ -89,50 +71,113 @@ def get_top_5_destinations(start_month: int, end_month: int, trip_type: str):
 
 
 def get_daily_plan_for_destination(arrival_date_and_time: str, departure_date_and_time: str, trip_type: str, location: str) -> str:
-    response = send_prompt2(f"""
+    prompt = f"""
     Given the arrival date and time {arrival_date_and_time}, departure date and time {departure_date_and_time}, trip type {trip_type}, and location {location}, generate a daily plan for a {trip_type} vacation in the given location.\n
     Do not include any other information in your response.
     The format should be:
     Day 1:
-    - <activity>
-    - <activity>
-    - <activity>
+    <activity>
+    <activity>
+    ...
     Day 2:
-    - <activity>
-    - <activity>
-    - <activity>
+    <activity>
+    <activity>
+    ...
     Day 3:
-    - <activity>
-    - <activity>
-    - <activity>
+    <activity>
+    <activity>
+    ...
     Day N:
-    - <activity>
-    - <activity>
-    - <activity>
+    <activity>
+    <activity>
+    ...
+    4 Best Moments:
+    <moment>
+    <moment>
+    <moment>
+    <moment>
     Where <activity> is an activity that can be done in the location or nearby, and N is the number of days in the trip.
     Do not include more than 3 activities per day.
     Make sure to include a variety of activities that cater to different interests and preferences.
     The activities in each day should be with consideration to the time it takes to do them, and so there could be days with less than 3 acivities.
     If the arrival date and time is in the evening, the first day should include activities that can be done in the evening, and the same applies to the departure date and time.
-    """)
+    In addition, each <moment> is a chosen activity (out of the activities you provided) which represents the best and beautiful moments of the trip.
+    """
+
+    prompt2 = f"""
+    I am going on a solo {trip_type} vacation to {location}. My arrival date and time: {arrival_date_and_time}, my departure date and time: {departure_date_and_time}.
+    I need you to generate a daily plan for this {trip_type} vacation in the given time and location.
+    Do not include any other information in your response.
+    The format should be:
+    Day 1:
+    <activity>
+    <activity>
+    ...
+    Day 2:
+    <activity>
+    <activity>
+    ...
+    Day 3:
+    <activity>
+    <activity>
+    ...
+    Day N:
+    <activity>
+    <activity>
+    ...
+    4 Best Moments:
+    <moment>
+    <moment>
+    <moment>
+    <moment>
+
+    Here are some guidance:
+    Each <activity> is an activity that can be done in the location or nearby at the specified time of year.
+    I put 2 activity tags in each day, but you can put less or more depending on the time it takes to do them.
+    N is the number of days in the trip.
+    Each <moment> is a chosen activity (out of the activities you provided) which represents the best and beautiful moments of the trip.
+
+    Some considerations:
+    Take into consideration the time of the day when the arrival and departure are.
+    Make sure to include a variety of activities that cater to different interests and preferences.
+    Also notice that like any other human - I need to relax, eat, and sleep. So make sure to include some relaxing activities and time to eat and sleep.
+    """
+
+    response = send_prompt(prompt)
     return response
+
+
+def find_airport_code(city: str, country: str) -> str:
+    with open('airports-code.json', 'r') as file:
+        airport_codes = json.load(file)
+    destination_airport_code = None
+    fixed_city = unicodedata.normalize('NFKD', city).encode(
+        'ascii', 'ignore').decode('utf-8')
+    fixed_country = unicodedata.normalize(
+        'NFKD', country).encode('ascii', 'ignore').decode('utf-8')
+    for airport in airport_codes:
+        if (fixed_city.lower().strip() in airport['city_name'].lower().strip()
+                or airport['city_name'].lower().strip() in fixed_city.lower().strip()
+                or fixed_city.lower().strip() in airport['airport_name'].lower().strip()) and \
+                (fixed_country.lower().strip() in airport['country_name'].lower().strip() or airport['country_name'].lower().strip() in fixed_country.lower().strip() or fixed_country.lower().strip() in airport['country_code'].lower().strip() or airport['country_code'].lower().strip() in fixed_country.lower().strip()):
+            destination_airport_code = airport['column_1']
+            break
+    if destination_airport_code:
+        return destination_airport_code
+    else:
+        return ''
 
 
 def search_flights(destinations: list, start_date: str, end_date: str, budget: int) -> dict:
     flights = {}
     for destination in destinations:
 
-        with open('airports-code.json', 'r') as file:
-            airport_codes = json.load(file)
-
         destination_city = destination[1]
         destination_country = destination[2]
         destination_airport_code = None
-        for airport in airport_codes:
-            if (destination_city.lower() in airport['city_name'].lower() or destination_city.lower() in airport['airport_name'].lower()) and (destination_country.lower() == airport['country_name'].lower() or destination_country.lower() in airport['country_code'].lower() or airport['country_code'].lower() in destination_country.lower()):
-                destination_airport_code = airport['column_1']
-                break
-        if not destination_airport_code:
+        destination_airport_code = find_airport_code(
+            destination_city, destination_country)
+        if destination_airport_code == '':
             print(f"Could not find airport code for {destination_city}, {destination_country}")  # nopep8
             continue
         from_TLV_flight_search_params = {
@@ -254,7 +299,7 @@ def search_hotels(destinations: dict, start_date: str, end_date: str) -> dict:
     return hotels
 
 
-def get_most_expensive_hotels(hotels: dict):
+def get_most_expensive_hotels(hotels: dict) -> dict:
     max_hotel = {}
     for key, hotels_and_leftover_budget in hotels.items():
         max_price = 0
@@ -268,10 +313,26 @@ def get_most_expensive_hotels(hotels: dict):
                 max_price = hotel_price
                 max_hotel[key] = hotel
 
-    if max_hotel is None:
-        return None
-
     return max_hotel
+
+
+def get_dalle_images(activity: str, country: str, gender: str) -> str:
+    response = OPENAI_CLIENT.images.generate(
+        model="dall-e-3",
+        # prompt=f"""Here is an activity that represents a beautiful moment of a trip to {country}: {activity}\nGenerate an image of the activity. This image should emphasise the true beauty of the trip, but make sure it looks real - don't exaggerate...\nIn addition, take into consideration that the person in the trip is traveling solo.""",  # nopep8
+        prompt=f"""I am a {gender}, and I am going to a solo vacation to {country}, and I am going to do the following activity: {activity}.
+        I could use your image generating skills to help me understand and feel the amazing moment I am going to have.
+        Please generate an image of the activity. Make sure it looks real - don't exaggerate...""",  # nopep8
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+
+    url = response.data[0].url
+    if url:
+        return url
+    else:
+        return ''
 
 
 def main():
@@ -300,12 +361,17 @@ def main():
 
     chosen_dest = input(
         "Enter the destination you would like to get a daily plan for: ")
+    gender = input("M/F: ")
     arrival_date = flights[chosen_dest][0][0]['flights'][len(flights[chosen_dest][0][0]['flights']) - 1]['arrival_airport']['time']  # nopep8
     print(arrival_date)
     departure_date = flights[chosen_dest][0][1]['flights'][0]['departure_airport']['time']  # nopep8
     print(departure_date)
-    print(get_daily_plan_for_destination(arrival_date, departure_date,
-          trip_type, f"{chosen_dest.split('@')[0]} , {chosen_dest.split('@')[2]}"))
+    daily_plan = get_daily_plan_for_destination(arrival_date, departure_date, trip_type, f"{chosen_dest.split('@')[0]} , {chosen_dest.split('@')[2]}")  # nopep8
+    print(daily_plan)
+    activities = daily_plan.splitlines()[-4:]
+    for activity in activities:
+        image_url = get_dalle_images(activity, chosen_dest.split('@')[2], gender)  # nopep8
+        print(image_url)
 
 
 if __name__ == "__main__":
